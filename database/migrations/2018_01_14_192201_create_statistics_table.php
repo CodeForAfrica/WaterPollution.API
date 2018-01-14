da1f44d6c8706933e46e5d915ad9eb16652c3b88
Timothy Anthony <anthony.timothy90@gmail.com>
2018-01-14 22:26:23 +0300

    Adding migration files

---
+++ database/migrations/2018_01_14_192201_create_statistics_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatisticsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statistics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('point_id');
+            $table->integer('cholera_level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statistics');
+    }
+}
